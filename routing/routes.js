9214965af1c5f99d8b49f43c3f5452e9bd2f76fb
SamHaggans <samuelhaggans@gmail.com>
2019-10-14 22:29:01 -0600

    Make users sign in for basically all things '

--- routing/routes.js
+++ routing/routes.js
@@ -18,51 +18,71 @@ module.exports = function(dirname) {
 	});
 	
 	router.get("/session-signup", function(req, res) {
-		res.sendFile(client_dir + "sessions.html", {root: dirname});
+        if (req.session.username) {
+            res.sendFile(client_dir + "sessions.html", {root: dirname});
+        }
+        else {
+            res.sendFile(client_dir + "illegal.html", {root: dirname});
+        }
 	});
 
 	router.get("/account", function(req, res) {
 		if (req.session.username) {
-			res.sendFile(client_dir + "account.html", {root: dirname});
-		}
-		else {
-			res.sendFile(client_dir + "nosign-account.html", {root: dirname});
-		}
+            res.sendFile(client_dir + "account.html", {root: dirname});
+        }
+        else {
+            res.sendFile(client_dir + "illegal.html", {root: dirname});
+        }
 	});
 	
 	router.get("/my-hours", function(req, res) {
 		if (req.session.username) {
-			res.sendFile(client_dir + "my-hours.html", {root: dirname});
-		}
-		else {
-			res.sendFile(client_dir + "nosign-my-hours.html", {root: dirname});
-		}
+            res.sendFile(client_dir + "my-hours.html", {root: dirname});
+        }
+        else {
+            res.sendFile(client_dir + "illegal.html", {root: dirname});
+        }
 	});
 
     router.get("/session/:id", function(req, res) {
         if (req.params.id == Number(req.params.id)) {
-            res.sendFile(client_dir + "session.html", {root: dirname});
+            if (req.session.username) {
+                res.sendFile(client_dir + "session.html", {root: dirname});
+            }
+            else {
+                res.sendFile(client_dir + "illegal.html", {root: dirname});
+            }
         }
         else if (req.params.id == "header.js" || req.params.id == "header.html"|| req.params.id == "signed-header.html"){
             res.sendFile(partials_dir + req.params.id, {root: dirname});
-		}
-		else {
-			res.sendFile(client_dir + req.params.id, {root: dirname});
-		}
+        }
+        else {
+            res.sendFile(client_dir + req.params.id, {root: dirname});
+        }
     });
 
-    router.get("/session/:id/:query", function(req, res) {
-        var leader = getSession(req.params.id).leader;
-            if (req.params.query == "attendance") {
-                res.sendFile(client_dir + "attendance.html", {root: dirname});
-            }
-            else if (req.params.query == "header.js" || req.params.query == "header.html"|| req.params.query == "signed-header.html"){
-                res.sendFile(partials_dir + req.params.query, {root: dirname});
+    router.get("/session/:id/:query", async function(req, res) {
+        if (req.params.query == "attendance") {
+            var leader = await getSession(req.params.id).leader;
+            var user = await getUser(req.session.user_id);
+            if (req.session.username) {
+                if (user.admin || leader == req.session.user_id) {
+                    res.sendFile(client_dir + "attendance.html", {root: dirname});
+                }
+                else {
+                    res.sendFile(client_dir + "illegal.html", {root: dirname});
+                }
             }
             else {
-                res.sendFile(client_dir + req.params.query, {root: dirname});
+                res.sendFile(client_dir + "illegal.html", {root: dirname});
             }
-        
+        }
+        else if (req.params.query == "header.js" || req.params.query == "header.html"|| req.params.query == "signed-header.html"){
+            res.sendFile(partials_dir + req.params.query, {root: dirname});
+        }
+        else {
+            res.sendFile(client_dir + req.params.query, {root: dirname});
+        }
     });
 
     var allowedFiles = ["main.js", "account.js", "header.js","login.js","merch.js","myhours.js","session.js","signup.js","style.js","favicon.ico","favicon2.ico"];
@@ -90,10 +110,67 @@ module.exports = function(dirname) {
 }
 
 
+function getUser(id) {
+    return new Promise(function(resolve, reject) {
+        var sql = "SELECT * FROM users WHERE id = ?";
+        con.query(sql, [id], function (err, result) {
+            if (err) throw err;
+            resolve(result[0]);
+        });
+    });  
+}
+
+function getSignupfromSessionUser(session_id, user_id) {
+    return new Promise(function(resolve, reject) {
+        var sql = "SELECT * FROM signups WHERE session_id = ? AND user_id = ?";
+        con.query(sql, [session_id, user_id], function (err, result) {
+            if (err) throw err;
+            if (result.length == 0) {
+                resolve(false);
+            }
+            else {
+                resolve(result[0]);
+            }
+        })
+    });
+}
+
+function markAttendance(session_id, user_id, attendance) {
+    return new Promise(function(resolve,reject) {
+        var sql = "UPDATE signups SET attendance = ? WHERE session_id = ? AND user_id = ?";
+        con.query(sql, [attendance, session_id, user_id], function(err, result) {
+            if (err) throw err;
+            else{
+                resolve(true);
+            }
+        })
+    })
+}
+
+function getSignupsBySession(session_id) {
+	return new Promise(function(resolve, reject) {
+        var sql = "SELECT * FROM signups WHERE session_id = ?";
+        con.query(sql, [session_id], function (err, result) {
+			if (err) throw err;
+            resolve(result);
+        })
+    });
+}
+
 function getSession(id) {
 	return new Promise(function(resolve, reject) {
         var sql = "SELECT * FROM sessions WHERE id = ?";
         con.query(sql, [id], function (err, result) {
+            if (err) throw err;
+            resolve(result[0]);
+        });
+    }); 
+}
+
+function getSessionByDate(year, month, day) {
+	return new Promise(function(resolve, reject) {
+        var sql = "SELECT * FROM sessions WHERE year = ? AND month = ? AND day = ?";
+        con.query(sql, [year, month, day], function (err, result) {
             if (err) throw err;
             resolve(result[0]);
         });
