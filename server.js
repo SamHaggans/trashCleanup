036320e20c9eacf053ed76261abf0f5de04137f2
SamHaggans <samuelhaggans@gmail.com>
2019-11-08 15:10:32 -0700

    Add loading in sessions from previous times

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ global.con = mysql.createConnection({
 con.connect(function (err) {
 	if (err) throw err;
 	console.log("Connected to database");
-	createSessions();
 });
 
 app.set('trust proxy', 1);
@@ -57,9 +56,35 @@ var server = app.listen(port, function () {
     console.log("Server started on port: localhost:%s", port);
 });
 
+loadPreviousSessions();
+setTimeout(function () {
+    createSessions();
+}, 5000);
+
 setInterval(function() {
     createSessions();
-  }, 1000 * 60 * 60 * 24);
+  }, 1000*10);
+
+async function loadPreviousSessions() {
+    var first = new Date(2019, 8, 2).getTime();
+    var current = new Date().getTime();
+    var increment = 1000 * 60 * 60 * 24;//ms => seconds => minutes => hours => 1 day
+    var newTime = first;
+    var day;
+    var month;
+    var year;
+    var i = 0;
+    while (newTime <= current) {
+        newTime = new Date(first + increment*i);
+		if (newTime.getDay() > 0 && newTime.getDay()<6) {
+			day = newTime.getDate();
+			month = newTime.getMonth();
+			year = newTime.getFullYear();
+            await createSession(year, month, day);
+        }
+        i++;
+    }
+}
 
 async function createSessions() {
     var current = new Date().getTime();
@@ -74,12 +99,12 @@ async function createSessions() {
 			day = newTime.getDate();
 			month = newTime.getMonth();
 			year = newTime.getFullYear();
-			await loadSession(year, month, day);
+			await createSession(year, month, day);
 		}
 	}
 }
 
-function loadSession (year, month, day) {
+function createSession(year, month, day) {
     return new Promise(function(resolve, reject) {
         var sql = "SELECT * FROM sessions WHERE year = ? AND month = ? AND day = ?";
         con.query(sql, [year,month,day], function(err, result) {
@@ -91,6 +116,13 @@ function loadSession (year, month, day) {
                     resolve();
                 }); 
             }
+            else {
+                resolve();
+            }
         })
     }); 
+}
+
+function sleep(ms) { //Sleep function for pauses between frames
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
